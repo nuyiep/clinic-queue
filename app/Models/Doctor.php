a981ecd86be27777a2439b6a0833da7062cc8cd0
Pei <nuyiep@hotmail.com>
2026-06-26 19:07:46 +0800

    Feat: Define Eloquent Model with mass assignment and relationships

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+		'id', 'name', 'specialty', 'is_active',
+	];
+
+	public function queues(): HasMany
+	{
+		return $this->hasMany(Queue::class);
+	}
+
+	public function consultations(): HasMany
+	{
+		return $this->hasMany(Consultations::class);
+	}
 }
